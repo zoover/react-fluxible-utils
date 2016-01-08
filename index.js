ca617daeb10d696c915b9b6db87dc632467af393
Gerben Meyer <g.meyer@zoover.nl>
2016-01-08 16:47:35 +0100

    transpile and lint

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  connectToStores: require('./lib/connectToStores'),
-  ReactSafeRender: require('./lib/reactSafeRender'),
-  MockDispatcher: require('./test/mockDispatcher'),
-  ShallowComponent: require('./test/shallowComponent')
-}
