5d1a253a8cf3ee875a544cf301ffa3f45c21c74d
Gerben Meyer <g.meyer@zoover.nl>
2016-01-13 16:19:38 +0100

    fix to selectFromObject

--- src/lib/selectFromObject.js
+++ src/lib/selectFromObject.js
@@ -1,14 +1,16 @@
-export default function (object, key, defaultValue) {
+const selectFromObject = function (object, key, defaultValue) {
   let _index;
   if (typeof object === 'undefined') {
     return defaultValue;
   }
   _index = key.indexOf('.');
   if (_index > -1) {
-    return this._fetchTranslation(object[key.substring(0, _index)], key.substr(_index + 1));
+    return selectFromObject(object[key.substring(0, _index)], key.substr(_index + 1));
   }
   if (object[key]) {
     return object[key];
   }
   return defaultValue;
 }
+
+export default selectFromObject;
