69488acb8a858ef7bcdfffa60df7496bc9f660c1
Gerben Meyer <g.meyer@zoover.nl>
2016-01-13 16:47:41 +0100

    fix oject undefined check

--- build/lib/selectFromObject.js
+++ build/lib/selectFromObject.js
@@ -5,12 +5,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 var selectFromObject = function selectFromObject(object, key, defaultValue) {
   var _index = undefined;
-  if (typeof object === 'undefined') {
+  if (!object) {
     return defaultValue;
   }
   _index = key.indexOf('.');
   if (_index > -1) {
-    return selectFromObject(object[key.substring(0, _index)], key.substr(_index + 1));
+    return selectFromObject(object[key.substring(0, _index)], key.substr(_index + 1), defaultValue);
   }
   if (object[key]) {
     return object[key];
--- src/lib/selectFromObject.js
+++ src/lib/selectFromObject.js
@@ -1,11 +1,11 @@
 const selectFromObject = function selectFromObject(object, key, defaultValue) {
   let _index;
-  if (typeof object === 'undefined') {
+  if (!object) {
     return defaultValue;
   }
   _index = key.indexOf('.');
   if (_index > -1) {
-    return selectFromObject(object[key.substring(0, _index)], key.substr(_index + 1));
+    return selectFromObject(object[key.substring(0, _index)], key.substr(_index + 1), defaultValue);
   }
   if (object[key]) {
     return object[key];
